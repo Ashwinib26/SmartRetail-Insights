1aff9f5a8abd407df5bd2c713a8cb1a20fa056d3
Ashwini Bisen <ashwinibisen26@gmail.com>
2025-05-23 23:58:47 +0530

    Data Preprocessing-2

--- data/preprocess.py
+++ data/preprocess.py
@@ -5,7 +5,15 @@ df = pd.read_csv('store.csv')
 # df.fillna(0, inplace=True)
 # df['Category'] = df['Category'].astype('category')
 
+print(df.size)
+print("Missing values per column:\n")
+print(df.isnull().sum())
+
+df.dropna(subset=['CompetitionDistance'], inplace=True)
 df.drop('Store',axis=1, inplace=True)
+
+print("Missing values per column:\n")
+print(df.isnull().sum())
 
 # Saved preprocessed data
 df.to_csv('cleaned_data.csv', index=False)
